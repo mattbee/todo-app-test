9ab1760ab1b60a15fc6a798809e7fa65500b7f0a
Matt Bee <matt@todayishould.com>
2019-11-23 20:46:59 +0000

    Add Todo Items display
    This commit adds a sample set of todos and a simple app to display todo
    items in the app.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoItem from '../components/TodoItem';
 import { formattedDate, isoDate } from '../helpers/dateHelpers.js';
 
-const TodoApp = () => (
-  <div>
-    <h1>Team To-Do List</h1>
-    <time dateTime={isoDate()}>
-      {formattedDate({ weekday: 'short', day: 'numeric', year: 'numeric', month: 'long'})}
-    </time>
-  </div>
-);
+const INITIAL_STATE = [
+  {
+    text: 'A todo item to be done',
+    id: '123'
+  },
+  {
+    text: 'Another todo not',
+    id: '123'
+  }
+];
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState(INITIAL_STATE);
+
+  return (
+    <div>
+      <h1>Team To-Do List</h1>
+      <time dateTime={isoDate()}>
+        {formattedDate({ weekday: 'short', day: 'numeric', year: 'numeric', month: 'long' })}
+      </time>
+
+      <section>
+        <ul>
+          {
+            todos.map((todo) => {
+              return <TodoItem key={todo.id} text={todo.text} />
+            })
+          }
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 export default TodoApp;
