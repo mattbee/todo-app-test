371a84e428b6eb5551ed465f7c36df7efcbea1dc
Matt Bee <matt@todayishould.com>
2019-11-24 20:26:39 +0000

    Add basic button component

---
+++ components/Button/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ children, onClick }) => (
+  <button onClick={onClick}>
+    {children}
+  </button>
+);
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func
+};
+
+export default Button;
