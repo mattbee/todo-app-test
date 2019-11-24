ff9def5889e81697fa12b0c0388052a05a98d61e
Matt Bee <matt@todayishould.com>
2019-11-24 20:14:01 +0000

    Meet ESLint rules for PropTypes
    Having added an extended set of ESLint rules and, using fixup,  added 
    them to the original commit introducing ESLint, this is the first time 
    my editor picked up missing PropTypes for the Todo Item component.
    
    This commit adds the PropTypes for text, the only prop at this time.

--- components/TodoItem/index.js
+++ components/TodoItem/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TodoItem = ({ text }) => (
   <li>{text}</li>
 );
+
+TodoItem.propTypes = {
+  text: PropTypes.string.isRequired
+};
 
 export default TodoItem;
